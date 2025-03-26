142c07e57b6322aab12d0df351e217ba2fbe524b
shreyas-omkar <shreyashegdeplus06@gmail.com>
2025-03-26 22:57:05 +0530

    Testing webhook trigger

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import { createNodeMiddleware, Probot } from "probot";
+import express from "express";
+import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+const privateKey = fs.readFileSync('./privateKey.pem', "utf8").trim();
+
+
+const probot = new Probot({
+  appId: Number(process.env.APP_ID), 
+  privateKey,
+  secret: process.env.WEBHOOK_SECRET,
+});
+
+const appFn = (app) => {
+  app.on(["push", "pull_request"], async (context) => {
+    try {
+      const { owner, repo } = context.repo();
+      console.log(`📡 Event received: ${context.name} for ${owner}/${repo}`);
+    } catch (error) {
+      console.error("❌ Error processing event:", error);
+    }
+  });
+};
+
+const app = express();
+app.use(createNodeMiddleware(appFn, { probot })); 
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
