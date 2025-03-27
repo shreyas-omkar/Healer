82d85ac420395a1f4c1391a44f035fa623f6d6b1
shreyas-omkar <shreyashegdeplus06@gmail.com>
2025-03-27 19:09:27 +0530

    Checking For Triggers

--- server/index.js
+++ server/index.js
@@ -5,16 +5,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-
+// Read private key
 const privateKey = fs.readFileSync('./privateKey.pem', "utf8").trim();
 
-
+// Initialize Probot
 const probot = new Probot({
   appId: Number(process.env.APP_ID), // Ensure it's a number
   privateKey,
   secret: process.env.WEBHOOK_SECRET,
 });
 
+// Express setup
 const app = express();
 app.use(express.json()); // Enable JSON parsing
 
