0e005218ef90b0aaaa55a657c7ca06b7301fa7e0
shreyas-omkar <shreyashegdeplus06@gmail.com>
2025-03-28 11:48:06 +0530

    Testing webhook

--- server/index.js
+++ server/index.js
@@ -12,10 +12,5 @@ app.use("/webhook", webhookRoutes);
 app.use("/api", apiRoutes);
 
 
-app.post("/", (req, res) => {
-  console.log("📡 Webhook received!", req.body);
-  res.status(200).send("✅ Webhook received!");
-});
-
 const PORT = 3000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
