cfc602359a39946e6217a490b8e4e4c1d3c3d585
shreyas-omkar <shreyashegdeplus06@gmail.com>
2025-03-28 21:46:52 +0530

    09:35PM

--- server/routes/webhookRoute.js
+++ server/routes/webhookRoute.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { probot } from "../probot.js";
-// import { webHook } from "../controllers/webHookController.js";
+import { webHook } from "../controllers/webHookController.js";
+
 const app = express();
 const router = express.Router();
 
@@ -10,13 +11,7 @@ app.use(express.json());
 // Middleware to parse URL-encoded bodies (in case the webhook sends data this way)
 app.use(express.urlencoded({ extended: true }));
 
-// Webhook Route to handle the incoming GitHub webhook
-router.post('/', (req, res) => {
-  console.log("Received Webhook Payload:", req.body); // Logs the parsed payload
-  res.status(200).send('Webhook received');
-});
-
-// Handle GitHub Webhook events (push and PR events)
+// Listen to PR & push events
 probot.webhooks.on(["push", "pull_request"], async (context) => {
   const { owner, repo } = context.repo();
   console.log(`📡 Event received: ${context.name} for ${owner}/${repo}`);
